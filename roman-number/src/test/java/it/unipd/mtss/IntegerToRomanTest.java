fbfeaaf6710c8b579f99b0a38ac5db1a10b0d574
val <valeriabaleanu@gmail.com>
2025-05-05 14:55:24 +0200

    close #9
    Aggiunta test per gestire le eccezioni di IntegerToRoman.java

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -4,3 +4,16 @@
 ////////////////////////////////////////////////////////////////////
 
 package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void TestExceptions() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(0));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(-1));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(1001));
+    }
+}
