11425368e423c844f6e0af020c45d96903e40995
Jen1315 <47667135+jen1315@users.noreply.github.com>
2025-05-05 19:00:51 +0200

    close #10

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -4,3 +4,21 @@
 ////////////////////////////////////////////////////////////////////
 
 package it.unipd.mtss;
+
+public class RomanPrinter {
+    /*
+    public static String print(int num){
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    */
+
+    private static String printAsciiArt(String romanNumber){
+        //TODO
+        return null;
+    }
+
+    /* Metodo accessore per test. Da rimuovere a release. */
+    public static String printAsciiArtTest(String romanNumber){
+        return printAsciiArt(romanNumber);
+    }
+}
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -4,3 +4,18 @@
 ////////////////////////////////////////////////////////////////////
 
 package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RomanPrinterTest {
+    
+    @Test
+    public void TestExceptions() {
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.printAsciiArtTest(""));
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.printAsciiArtTest("ABCD"));
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.printAsciiArtTest("1234"));
+        assertThrows(IllegalArgumentException.class, () -> RomanPrinter.printAsciiArtTest("?!&/"));
+    }
+}
