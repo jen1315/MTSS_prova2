c83590a53933b4ff108076128603706050420bbf
Jen1315 <47667135+jen1315@users.noreply.github.com>
2025-05-06 20:52:27 +0200

    close #22
    Implementazione codice per risolvere test

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -16,16 +16,52 @@ public class RomanPrinter {
       if(romanNumber=="") { 
         throw new IllegalArgumentException("String can't be empty"); 
       }
-      char[] rm = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+      String art[][] = new String[10][6];
       for(int i=0; i<romanNumber.length(); i++) {
         char c = romanNumber.charAt(i);
-        for(int j=0; i<rm.length; j++) {
-          if(c!=rm[j]) {
+        if(c=='I') { art[i]=printAscii_I(); }
+        else if(c=='V') { art[i]=printAscii_V(); }
+        else if(c=='X') { art[i]=printAscii_X(); }
+        else {
             throw new IllegalArgumentException("String must be roman number"); 
-          }
         }
       }
-        return null;
+      String out = "";
+      for(int z=0; z<6; z++) {
+        for(int w=0; w<art.length; w++) {
+          if(art[w][z]==null) {out += "";}
+          else {out += art[w][z];}
+        }
+        out += "\n";
+      }
+      return out;
+    }
+
+    private static String[] printAscii_I() {
+        return new String[] {" _____  ",
+                             "|_   _| ",
+                             "  | |   ",
+                             "  | |   ",
+                             " _| |_  ",
+                             "|_____| "};
+    }
+
+    private static String[] printAscii_V() {
+        return new String[] {"__      __ " ,
+                             "\\ \\    / / " ,
+                             " \\ \\  / /  " ,
+                             "  \\ \\/ /   " ,
+                             "   \\  /    " ,
+                             "    \\/     ", };
+    }
+
+    private static String[] printAscii_X() {
+        return new String[] {"__    __ " ,
+                             "\\ \\  / / " ,
+                             " \\ \\/ /  " ,
+                             "  >  <   " ,
+                             " / /\\ \\  " ,
+                             "/_/  \\_\\ " };
     }
 
     /* Metodo accessore per test. Da rimuovere a release. */
