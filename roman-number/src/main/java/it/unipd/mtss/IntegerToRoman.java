7723246ef80f514b7c135fbd379db965fb7f7790
val <valeriabaleanu@gmail.com>
2025-05-06 14:59:55 +0200

    closes #42, closes #43, closes #40
    Modifica della funzione in IntegerToRoman per passare il Test di conversione dei primi 500 numeri interi. Non è necessario refactoring poiché il codice non è ulteriormente migliorabile. Termine dell'implementazione del test relativo ai primi 500 numeri interi.

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -13,8 +13,8 @@ public class IntegerToRoman {
         }
 
         // Corrispondenza numeri romani - arabi 'speciali'
-        String[] roman = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int[] arab = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = {"D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] arab = {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         //Per memorizzare il risultato
         StringBuilder result = new StringBuilder();
