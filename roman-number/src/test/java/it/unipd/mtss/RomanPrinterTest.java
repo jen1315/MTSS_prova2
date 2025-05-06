86c6e2e092b6f6862f2bdc453763b9a9be3bdc08
Jen1315 <47667135+jen1315@users.noreply.github.com>
2025-05-07 01:12:41 +0200

    close #29
    Stesura di test per la corretta stampa dell'ascii art di numeri romani minori e uguali a 50

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -86,4 +86,32 @@ public class RomanPrinterTest {
                      " / /\\ \\  \n" +
                      "/_/  \\_\\ \n", RomanPrinter.printAsciiArtTest("X"));
     }
+
+    @Test
+    public void TestPrintAscii_to50() {
+        assertEquals("__    __  _____  __    __ \n" +
+                     "\\ \\  / / |_   _| \\ \\  / / \n" +
+                     " \\ \\/ /    | |    \\ \\/ /  \n" +
+                     "  >  <     | |     >  <   \n" +
+                     " / /\\ \\   _| |_   / /\\ \\  \n" +
+                     "/_/  \\_\\ |_____| /_/  \\_\\ \n", RomanPrinter.printAsciiArtTest("XIX"));
+        assertEquals("__    __ __    __ __    __ __      __  _____   _____   _____  \n" +
+                     "\\ \\  / / \\ \\  / / \\ \\  / / \\ \\    / / |_   _| |_   _| |_   _| \n" +
+                     " \\ \\/ /   \\ \\/ /   \\ \\/ /   \\ \\  / /    | |     | |     | |   \n" +
+                     "  >  <     >  <     >  <     \\ \\/ /     | |     | |     | |   \n" +
+                     " / /\\ \\   / /\\ \\   / /\\ \\     \\  /     _| |_   _| |_   _| |_  \n" +
+                     "/_/  \\_\\ /_/  \\_\\ /_/  \\_\\     \\/     |_____| |_____| |_____| \n", RomanPrinter.printAsciiArtTest("XXXVIII"));
+        assertEquals("__    __  _        _____  __    __ \n" +
+                     "\\ \\  / / | |      |_   _| \\ \\  / / \n" +
+                     " \\ \\/ /  ! |        | |    \\ \\/ /  \n" +
+                     "  >  <   | |        | |     >  <   \n" +
+                     " / /\\ \\  | |____   _| |_   / /\\ \\  \n" +
+                     "/_/  \\_\\ |______| |_____| /_/  \\_\\ \n", RomanPrinter.printAsciiArtTest("XLIX"));
+        assertEquals(" _       \n" +
+                     "| |      \n" +
+                     "! |      \n" +
+                     "| |      \n" +
+                     "| |____  \n" +
+                     "|______| \n", RomanPrinter.printAsciiArtTest("L"));
+    }
 }
